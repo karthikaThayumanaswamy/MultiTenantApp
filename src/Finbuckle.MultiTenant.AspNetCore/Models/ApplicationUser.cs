53f09a058a3f4f6def56a7fc6cbe0aa650d11ec8
karthikaThayumanaswamy <48909907+karthikaThayumanaswamy@users.noreply.github.com>
2024-05-08 15:20:37 +0530

    Delete src directory

--- src/Finbuckle.MultiTenant.AspNetCore/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Finbuckle.MultiTenant.AspNetCore.Models;
-
-public class ApplicationUser : IdentityUser
-{
-    public string TenantId { get; set; } = "D2FA78CE-3185-458E-964F-8FD0052B4330";
-}
--- src/Finbuckle.MultiTenant.AspNetCore/Options/TenantOptions.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace Finbuckle.MultiTenant.AspNetCore.Options;
-
-public class TenantOptions
-{
-    public List<AppTenantInfo> Tenants { get; set; } 
-}
