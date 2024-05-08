f798e811d23c1db483c1c6876f2144b89ea45399
karthikaThayumanaswamy <48909907+karthikaThayumanaswamy@users.noreply.github.com>
2024-05-08 15:28:31 +0530

    Delete test directory

--- test/Finbuckle.MultiTenant.AspNetCore.Test/MultiTenantRouteBuilderShould.cs
+++
@@ -1,4 +0,0 @@
-// Copyright Finbuckle LLC, Andrew White, and Contributors.
-// Refer to the solution LICENSE file for more information.
-
-// Note: MultiTenantRouteBuilder is a trivial tweak of Microsoft's RouteBuilder implementation.
